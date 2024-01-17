4ac76b570ba1273f7a219b12ceb0e1cb93bef7d4
CesarSBastos <cesarbastos32@gmail.com>
2024-01-17 12:15:45 -0300

    First Commit

---
+++ js/script.js
@@ -0,0 +1,26 @@
+const container = document.querySelector('.container'),
+qrInput = container.querySelector('.form input'),
+generateBtn = container.querySelector('.form button'),
+qrImg = container.querySelector('.qr-code .img'),
+qrDownload = container.querySelector('.qr-code .a');
+
+generateBtn.addEventListener('click', () => {
+    let qrValue = qrInput.value;
+    if(!qrValue){
+        alert('Insira uma URL ou texto para gerar um QR Code')
+        return;
+    }
+    generateBtn.innerText = "Gerando um QR Code...";
+    qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=170x170&data=${qrValue}`;
+    qrDownload.href = `https://api.qrserver.com/v1/create-qr-code/?size=170x170&data=${qrValue}`;
+    qrImg.addEventListener('load', () => {
+        generateBtn.innerText = "Gerar QR Code";
+        container.classList.add('active');
+    })
+})
+
+qrInput.addEventListener('keyup', () => {
+    if(!qrInput.value){
+        container.classList.remove('active');
+    }
+})
